a1e013440036ada175deb0027216e77c498865d6
Stephan Jorek <stephan.jorek@gmail.com>
2017-11-03 14:50:55 +0100

    composer ports use indiviual COMPOSER_HOME but common COMPOSER_CACHE_DIR now

--- php/composer/files/composer.sh
+++ php/composer/files/composer.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-@PHP@ @LIBPATH@/composer.phar $@
+# we use an individual home-dir, but a common cache-dir
+COMPOSER_HOME=${COMPOSER_HOME:-${HOME}/.composer@VARIANT@} \
+COMPOSER_CACHE_DIR=${COMPOSER_CACHE_DIR:-${HOME}/.composer/cache} \
+    @PHP@ @LIBPATH@/composer.phar $@
