38ef9872ada9a8689b72ff55c09ad7d3457ca0e7
Stephan Jorek <stephan.jorek@brandung.de>
2019-06-26 11:25:08 +0200

    [TASK] switch to php-based composer shell wrapper

--- php/composer/files/composer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# @NAME@                                                    -*- shell-script -*-
-
-# we use an individual home-dir, but a common cache-dir
-COMPOSER_HOME=${COMPOSER_HOME:-${HOME}/.composer@VARIANT@} \
-COMPOSER_CACHE_DIR=${COMPOSER_CACHE_DIR:-${HOME}/.composer/cache} \
-    @PHP@ @LIBPATH@/composer.phar $@
